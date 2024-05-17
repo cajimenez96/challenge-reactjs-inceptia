83d6b5ab5fb546efdde01e53e906d9a3431590bd
cajimenez96 <carlosj_utn@hotmail.com>
2024-05-16 21:38:04 -0300

    Update react version

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import "babel-polyfill";
 import "react-app-polyfill/ie11";
 
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
