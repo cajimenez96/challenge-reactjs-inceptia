599363b2fd667aa0f362fa88f7ff4b6a2322f06e
Carlos Jimenez <cjimenez@totalcoin.com>
2024-05-19 17:11:18 -0300

    Settings colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
   theme: {
+    colors: {
+      'bg-primary': 'var(--bg-primary)',
+      'bg-secondary': 'var(--bg-secondary)',
+      'bg-box': 'var(--bg-box)',
+      'font-primary': 'var(--font-primary)',
+      'font-secondary': 'var(--font-secondary)',
+      'border-primary': 'var(--border-primary)'
+    },
     extend: {},
   },
   plugins: [],
