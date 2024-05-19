d4af8fef048842bca4f965cd583d545e2825e05d
cajimenez96 <carlosj_utn@hotmail.com>
2024-05-18 23:40:12 -0300

    Update request details

---
+++ frontend/src/api/Reports.ts
@@ -0,0 +1,44 @@
+import Axios from "../lib/axios";
+import { Bot, Client, Case_Log, Case_Result, Extra_Metadata } from "./interface";
+
+export type GetClientsResponse = Client[];
+
+export interface Case {
+  bot: Bot;
+  case_duration: string;
+  case_log: Case_Log;
+  case_result: Case_Result;
+  case_uuid: string;
+  code: string | null;
+  extra_metadata: Extra_Metadata;
+  first_name: string;
+  id: number;
+  is_active: boolean;
+  is_complete: boolean;
+  last_name: string;
+  last_updated: string;
+  phone: number;
+  recording: string;
+  status: string;
+}
+
+export interface GetDetailsResponse {
+  count: number;
+  previus: string | null;
+  next: string | null;
+  results: Case[];
+}
+
+export const GetClientsRequest = async (): Promise<GetClientsResponse[]> => {
+  const response = await Axios.get('/clients/');
+  return response.data;
+};
+
+export const GetClientDetailRequest = async (
+  id: number,
+  from: string,
+  to: string
+): Promise<GetDetailsResponse> => {
+  const response = await Axios.get(`/inbound-case/?bot=${id}&local_updated__date__gte=${from}&local_updated__date__lte=${to}`);
+  return response.data;
+};
--- frontend/src/pages/Report/index.tsx
+++ frontend/src/pages/Report/index.tsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Sidebar from '../../components/Sidebar';
+import Table from '../../components/Table';
+import { getClientDetail, getClientReport } from './require';
+import { Client } from '../../api/interface';
+import { Case } from '../../api/Reports';
+import Filter from '../../components/Filter';
 
-type Props = {}
+interface Time {
+  startDate: string | null;
+  endDate: string | null;
+}
 
-const Report = (props: Props) => {
+const INITIAL_STATE = {
+  startDate: "2021-03-01",
+  endDate: "2022-03-25"
+}
+
+const Report = () => {
+  const [clientDetail, setClientDetail] = useState<Case[]>([]);
+  const [clientId, setClientId] = useState<number>(0);
+  const [users, setUsers] = useState<Client[]>([]);
+  const [filterDate, setFilterDate] = useState<Time>(INITIAL_STATE); 
+
+  useEffect(() => {
+    const getClients = async () => {
+      await getClientReport()
+      .then((res) => {
+        setUsers(res);
+      })
+      .catch((err) => {
+        console.log('todo mal' + err);
+      });
+    }
+
+    getClients();
+  }, []);
+
+  useEffect(() => {
+    const getDetails = async (id: number, filter: Time) => { 
+
+      await getClientDetail(id, filter.startDate, filter.endDate)
+      .then((res) => {
+        setClientDetail(res.results)
+      })
+      .catch((err) => {
+        console.log('todo mal' + err);
+      })
+    }
+
+    if (clientId) getDetails(clientId, filterDate);
+  }, [clientId, filterDate]);
+
   return (
-    <div>index</div>
+    <Sidebar elements={users} setElement={setClientId}>
+      <section>
+        <article className="mb-10 mt-5">
+          <h3 className="uppercase">Reportes</h3>
+        </article>
+        <article className="mb-10 w-full">
+          <div className="flex justify-end items-center gap-4">
+            <p>Filtrar</p>
+            <div className=" w-80">
+              <Filter date={filterDate} setDate={setFilterDate} />
+            </div>
+          </div>
+        </article>
+        <article>
+          <Table elements={clientDetail}/>
+        </article>
+      </section>
+    </Sidebar>
   )
 }
 
