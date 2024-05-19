2a76b993e1fa5f0af184859f1ccfd6146a674199
Carlos Jimenez <cjimenez@totalcoin.com>
2024-05-19 18:43:09 -0300

    update settings

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
   ],
   theme: {
     colors: {
-      'bg-primary': 'var(--bg-primary)',
-      'bg-secondary': 'var(--bg-secondary)',
-      'bg-box': 'var(--bg-box)',
-      'font-primary': 'var(--font-primary)',
-      'font-secondary': 'var(--font-secondary)',
+      'bg-light': 'var(--bg-light)',
+      'bg-blue': 'var(--bg-blue)',
+      'bg-dark': 'var(--bg-dark)',
+      'bg-white': 'var(--bg-white)',
+      'font-light': 'var(--font-light)',
+      'font-dark': 'var(--font-dark)',
+      'font-white': 'var(--font-white)',
       'border-primary': 'var(--border-primary)',
-      'border-secondary': 'var(--bg-secondary)'
+      'border-secondary': 'var(--bg-blue)',
+      'transparent': 'transparent',
+      'hover': 'var(--bg-terciary-hover)'
     },
     extend: {},
   },
