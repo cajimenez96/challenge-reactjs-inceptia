b0774d8abea119cd6d7a5c737ba77ec8de8a6932
cajimenez96 <carlosj_utn@hotmail.com>
2024-05-18 23:39:10 -0300

    Add filter

---
+++ frontend/src/components/Filter/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Datepicker, { DateValueType } from "react-tailwindcss-datepicker";
+
+interface Time {
+  startDate: string | null;
+  endDate: string | null;
+}
+
+interface FilterProps {
+  date: Time;
+  setDate: (value: Time) => void;
+}
+
+const INITIAL_STATE = {
+  startDate: "2021-03-01",
+  endDate: "2022-03-25"
+}
+
+const Filter = ({setDate, date}: FilterProps) => {
+    
+    const handleValueChange = (newValue: any | Time) => {
+    if (newValue.endDate === null || newValue.startDate === null) setDate(INITIAL_STATE)
+    else setDate(newValue); 
+    } 
+
+  return (
+      <Datepicker
+        value={date}
+        onChange={handleValueChange}
+        displayFormat={"YYYY-MM-DD"}
+        separator='hasta'
+      /> 
+  )
+}
+
+export default Filter;
