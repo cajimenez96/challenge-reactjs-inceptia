53ad2d0ab4374e46e188e9d76356583912124371
cajimenez96 <carlosj_utn@hotmail.com>
2024-05-22 15:17:00 -0300

    Deleted comments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,7 @@
-// import "babel-polyfill";
-// import "react-app-polyfill/ie11";
-
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// // import * as serviceWorker from "./serviceWorker";
-// import App from "./App";
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     <App/>
-//   </React.StrictMode>)
-
-// // serviceWorker.unregister();
-
 import './index.css';
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
-
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
